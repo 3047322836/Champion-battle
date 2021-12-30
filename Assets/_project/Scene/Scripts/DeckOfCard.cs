a8e13a74a1c1e101e5d2f981a10f44ef754fa964
williamluo888 <williamluo888@gmail.com>
2021-12-29 21:26:18 -0800

    coded-uplication

--- Assets/_project/Scene/Scripts/DeckOfCard.cs
+++ Assets/_project/Scene/Scripts/DeckOfCard.cs
@@ -7,15 +7,17 @@ public class DeckOfCard : MonoBehaviour
     public List<GameObject> OriginalCards = new List<GameObject>();
     public List<GameObject> cards = new List<GameObject>();
     public int count;
-
+    public Canvas canvas;
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < count; i++)
         {
             int whichCard = i % OriginalCards.Count;
-            GameObject card = Instantiate(OriginalCards[i]); //to make an example of an original card
+            GameObject card = Instantiate(OriginalCards[whichCard]); //to make an example of an original card
+            card.transform.SetParent(canvas.transform);
             cards.Add(card);
+            card.transform.position = Vector3.up * i * 301.0f;
         }
         
     }
