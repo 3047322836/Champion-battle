ff9cbb2f4b0cf42f7114a90dc1992afe5e8793df
cashmoneykristen <k.ponleithner1@gmail.com>
2022-01-07 21:55:38 -0800

    Update DeckOfCard.cs

--- Assets/_project/Scene/Scripts/DeckOfCard.cs
+++ Assets/_project/Scene/Scripts/DeckOfCard.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeckOfCard : MonoBehaviour
 {
     public List<GameObject> OriginalCards = new List<GameObject>();
     public List<GameObject> cards = new List<GameObject>();
+    public Sprite[] icons;
     public int count;
     public Canvas canvas;
 
@@ -17,8 +19,13 @@ public class DeckOfCard : MonoBehaviour
         {
             int whichCard = i % OriginalCards.Count;
             GameObject card = Instantiate(OriginalCards[whichCard]); //to make an example of an original card
+ 
+            Debug.Log(card.name);
+            card.GetComponent<BasicCard>().icon = icons[0];
+            card.GetComponent<BasicCard>().num = 42.ToString(); //"42";
             card.transform.SetParent(canvas.transform);
             cards.Add(card);
+            //Debug.Log(card.GetComponent<BasicCard>().img_icon?.sprite?.name ?? "nothing");
             card.transform.position = Vector3.up * i * 305.0f;
         }
         
